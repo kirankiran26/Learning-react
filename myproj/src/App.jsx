510e0e169f061ab3b63d57a12a034adc8c40ede6
Kiran P S <kiranps2607@gmail.com>
2024-06-14 23:15:27 +0530

    Adding files

---
+++ myproj/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Biling from './Biling'
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+   <>
+   <Biling/>
+   </>
+  );
+}
+
+export default App
