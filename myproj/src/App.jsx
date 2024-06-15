1bb050274aeaf9b996f21d0ea9d950ad421e805c
Kiran P S <kiranps2607@gmail.com>
2024-06-15 18:11:19 +0530

    usestate

--- myproj/src/App.jsx
+++ myproj/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import PropsAndCOmp from './PropsAndCOmp/PropsAndCOmp'
+import UseState from './PropsAndCOmp/HooksInReact/UseState1'
 function App() {
   return (
    <>
-   <PropsAndCOmp/>
+   <UseState/>
    </>
   );
 }
